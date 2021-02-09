8461be13293ab98308e44c250fb58c473f16345f
sagari <sagari2103@gmail.com>
2021-02-09 00:02:19 -0800

    TodoList-2

--- To-dolist-Project/todo.js
+++ To-dolist-Project/todo.js
@@ -1,3 +1,34 @@
-function myFunction(){
-    document.getElementById("demo").innerHTML = "Success!";
+var input = document.getElementById("userinput");
+var ul = document.querySelector("ul");
+var button = document.getElementById("enter");
+
+function inputLength(){
+    return input.value.length;
 }
+
+function createNewListItem(){
+    var li = document.createElement("li");
+    li.appendChild(document.createTextNode
+        (input.value));
+    ul.appendChild(li);  
+    input.value = "";  
+    }
+
+function addListAfterClick() {
+    if (inputLength() > 0) {
+        createNewListItem();
+        }
+    else{
+        alert("Please enter something!");
+    }    
+    }    
+
+function addListAfterKeypress(e){
+    if(inputLength() > 0 && e.which === 13){
+        createNewListItem();
+    }
+}
+
+input.addEventListener("keypress",addListAfterKeypress);
+
+button.addEventListener("click", addListAfterClick);
