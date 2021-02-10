013f3ec80a49921134fdd9dabc9d84247e293705
sagari <sagari2103@gmail.com>
2021-02-10 15:17:40 -0800

    TodoList-4

--- To-dolist-Project/todo.js
+++ To-dolist-Project/todo.js
@@ -1,33 +1,27 @@
-var input = document.getElementsByClassName("inputField");
-var addBtn = document.querySelector(".inputField button");
+
+var input = document.getElementById("user_input");
+var addBtn = document.getElementById("add");
 var ul = document.querySelector("ul");
 
 function inputLength(){
     return input.value.length;
 }
 
-function createListElement(){
-    var li = document.createElement("li");
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = "";
+function createListElement() {
+	var li = document.createElement("li");
+	li.appendChild(document.createTextNode(input.value));
+	ul.appendChild(li);
+	input.value = "";
 }
 
-function addListAfterClick(){
-    if(inputLength() > 0){
-        createListElement();
-    }
-    else{
-        alert("Please enter something to add!");
-    }
+
+function addListAfterClick() {
+	if (inputLength() > 0) {
+		createListElement();
+	}
 }
 
-function addListAfterKeypress(ev){
-    if(inputLength() > 0 && ev.keycode === 13){
-        createListElement();
-    }
 
-}
 
 // onkeyup event
 // input.onkeyup() = ()=> {
@@ -39,7 +33,22 @@ function addListAfterKeypress(ev){
 //     }
 // }
 
+function addListAfterKeypress(event) {
+	if (inputLength() > 0 && event.keyCode === 13) {
+		createListElement();
+	}
+}
+
 addBtn.addEventListener("click",addListAfterClick);
 
-
 input.addEventListener("keypress",addListAfterKeypress);
+// click on the trash icon to hide the current list item
+
+var trash = document.getElementsByClassName("far");
+
+for(var i=0;i < trash.length;i++){
+    trash[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
